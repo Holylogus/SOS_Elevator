c7a366f256a04d2aa3729f41e15f61d788bfa93b
Holylogus <hamar.patrik94@gmail.com>
2024-11-18 14:55:26 +0100

    Contact + Footer ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
+import "../styles/Footer.css"
+
 export default function Footer() {
     return (
         <footer id="Footer" >
-            
+            <div className="legacy"><p>Minden jog fentartva az SOS Elevator Kft. számára </p></div>
+            <div className="author">
+                <p>Website by: Hamar Patrik</p>
+                <p>email: hamar.patrik94@gmail.com</p>
+            </div>
         </footer>
     )
 }

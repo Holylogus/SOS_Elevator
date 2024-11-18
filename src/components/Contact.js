354670acc9fe8ea21d24d7db06dc400125cd8ce7
Holylogus <hamar.patrik94@gmail.com>
2024-11-18 14:42:23 +0100

    Contact Section ready

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,48 @@
+
+import "../styles/Contact.css";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+
 export default function Contact() {
-    return(
-        <div id="Contact" >
-            
-        </div>
-    )
+  const mapStyles = {
+    height: "100%",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 47.33870753623736,
+    lng: 19.23393734847873,
+  };
+
+  return (
+    <div id="Contact">
+      <div className="brand">
+        <h1>SOS Elevator</h1>
+        <button>Ajánlatkérés</button>
+      </div>
+      <div className="contact">
+        <h3>Kapcsolat</h3>
+        <p>
+          <strong>Email: </strong>soselevator@gmail.com
+        </p>
+        <p>
+          <strong>Tel.: </strong>+36 30 502 18 35
+        </p>
+        <p>
+          <strong>Székhely: </strong>2363 Felsőpakony, Madách Tér 3/2
+        </p>
+      </div>
+      <div className="googleMap">
+        <LoadScript googleMapsApiKey="AIzaSyDHfNV303jB4lwEdpo882ddUkrvEP0oaLg">
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            center={defaultCenter}
+            zoom={17}
+          >
+            <Marker position={defaultCenter} />
+          </GoogleMap>
+        </LoadScript>
+      </div>
+    </div>
+  );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -7,11 +7,11 @@ export default function Info() {
     return(
         <div id="Info" >
             <div className="phone">
-                <img src={phoneIcon}/>
+                <img src={phoneIcon} alt="phoneIcon"/>
                 <p>+36 30 502 18 35</p>
             </div>
             <div className="email">
-            <Image src={mailIcon}/>
+            <Image src={mailIcon} alt="mailIcon"/>
             <p>soselevator@gmail.com</p>
             </div>
         </div>

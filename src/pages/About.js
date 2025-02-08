7df2220b8c75841eec68b1a4fde895fc30b381e8
Holylogus <hamar.patrik94@gmail.com>
2025-02-08 18:39:22 +0100

    About Page respomsove design done

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,65 +7,63 @@ import customer from "../icons/customer.png";
 import { useEffect, useRef } from "react";
 
 export default function About() {
-
   const aboutSectionRef = useRef(null);
   const valueTitleRef = useRef(null);
   const valueSectionRef = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const observer = new IntersectionObserver(
-      (entries)=>{
-        entries.forEach((entry)=>{
+      (entries) => {
+        entries.forEach((entry) => {
           if (entry.isIntersecting) {
             entry.target.classList.add("animate");
             observer.unobserve(entry.target);
           }
         });
       },
-      {threshold: 0.2}
+      { threshold: 0.2 }
     );
 
     if (aboutSectionRef.current) {
-      const elementsToAnimate = aboutSectionRef.current.querySelectorAll(".slideInRight");
-      elementsToAnimate.forEach((el)=>observer.observe(el));
-    };
+      const elementsToAnimate =
+        aboutSectionRef.current.querySelectorAll(".slideInRight");
+      elementsToAnimate.forEach((el) => observer.observe(el));
+    }
 
     if (valueTitleRef.current) {
       observer.observe(valueTitleRef.current);
     }
 
     if (valueSectionRef.current) {
-      const elementsToAnimate = valueSectionRef.current.querySelectorAll(".fadeInUp");
-      elementsToAnimate.forEach((el)=> observer.observe(el));
+      const elementsToAnimate =
+        valueSectionRef.current.querySelectorAll(".fadeInUp");
+      elementsToAnimate.forEach((el) => observer.observe(el));
     }
 
-    return()=>observer.disconnect();
-  },[])
+    return () => observer.disconnect();
+  }, []);
 
   return (
-    <div id="AboutPage" >
+    <div id="AboutPage">
       <div className="aboutSection" ref={aboutSectionRef}>
-        <img className="mySelfImage" src="#" alt="myself" />
+        {/* <img className="mySelfImage" src="#" alt="myself" /> */}
         <h2 className="aboutTitle slideInRight delay-1">Rólunk:</h2>
         <div className="aboutContent">
-        <p className="slideInRight delay-2">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Congue hendrerit
-          nec, odio fringilla lacus ad. Nascetur condimentum penatibus fermentum
-          rhoncus ad eleifend, per volutpat tincidunt. Pellentesque erat montes
-          ac urna massa odio dolor. Ainteger magna massa laoreet dignissim
-          maecenas potenti. Sit luctus mollis mollis torquent parturient tempor
-          gravida. Accumsan class tempus senectus quis nascetur suscipit
-          convallis.
-        </p>
-        <p className="slideInRight delay-3">
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Congue hendrerit
-          nec, odio fringilla lacus ad. Nascetur condimentum penatibus fermentum
-          rhoncus ad eleifend, per volutpat tincidunt. Pellentesque erat montes
-          ac urna massa odio dolor. Ainteger magna massa laoreet dignissim
-          maecenas potenti. Sit luctus mollis mollis torquent parturient tempor
-          gravida. Accumsan class tempus senectus quis nascetur suscipit
-          convallis.
-        </p>
+          <p className="slideInRight delay-2">
+            Az <strong>SOS Elevator Kft.</strong> több mint egy évtizede biztosít megbízható és
+            professzionális megoldásokat a liftek és mozgólépcsők karbantartása,
+            szervizelése és modernizációja területén. Cégünk elkötelezett
+            amellett, hogy a legmagasabb színvonalú szolgáltatást nyújtsa
+            ügyfelei számára, kiemelt figyelmet fordítva a biztonságra,
+            pontosságra és az egyedi igényekre.
+          </p>
+          <p className="slideInRight delay-3">
+            Szakképzett csapatunk a legmodernebb technológiák alkalmazásával
+            segít meghosszabbítani a liftek és mozgólépcsők élettartamát,
+            minimalizálva az állásidőt és maximalizálva az energiahatékonyságot.
+            Az általunk végzett munkára garanciát vállalunk, biztosítva, hogy
+            rendszerei hosszú távon is megbízhatóan működjenek.
+          </p>
         </div>
       </div>
       <h2 className="fadeInUp delay-1">Értékeink:</h2>
@@ -92,6 +90,5 @@ export default function About() {
         </div>
       </div>
     </div>
-    
   );
 }
